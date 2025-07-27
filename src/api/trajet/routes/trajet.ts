ea2d85662599ae24aaee7f8c043f4f32f0fe02dd
iamyannick237 <iamyannick237Ggmail.com>
2025-07-27 03:22:50 -0700

    Modification du code de la route de trajet

--- src/api/trajet/routes/trajet.ts
+++ src/api/trajet/routes/trajet.ts
@@ -1,7 +1,10 @@
-/**
- * trajet router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::trajet.trajet');
+export default [
+  {
+    method: 'GET',
+    path: '/trajets/:id',
+    handler: 'api::trajet.trajet.findOne',
+    config: {
+      auth: false, // à mettre à true si tu veux restreindre
+    },
+  },
+];
