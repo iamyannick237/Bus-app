2511c03f0a3613344f880380c0865cdc0953c590
iamyannick237 <iamyannick237Ggmail.com>
2025-07-27 03:41:26 -0700

    REMODIF

--- src/api/trajet/controllers/trajet.ts
+++ src/api/trajet/controllers/trajet.ts
@@ -1,18 +1,9 @@
+ /**
+ * trajet router
+ */
+
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::trajet.trajet', ({ strapi }) => ({
-  async findByDocumentId(ctx) {
-    const { documentId } = ctx.params;
+export default factories.createCoreRouter('api::trajet.trajet');
 
-    const trajet = await strapi.entityService.findMany('api::trajet.trajet', {
-      filters: { documentId: documentId },
-      limit: 1,
-    });
 
-    if (!trajet || trajet.length === 0) {
-      return ctx.notFound('Trajet non trouvé');
-    }
-
-    return trajet[0];
-  },
-}));
--- src/api/trajet/routes/trajet.ts
+++ src/api/trajet/routes/trajet.ts
@@ -5,19 +5,3 @@
 import { factories } from '@strapi/strapi';
 
 export default factories.createCoreRouter('api::trajet.trajet');
-
-const customRoutes = [
-  {
-    method: 'GET',
-    path: '/trajets/document/:documentId',
-    handler: 'trajet.findByDocumentId',
-    config: {
-      auth: false,
-    },
-  },
-];
-
-export default {
-  ...defaultRouter,
-  routes: [...defaultRouter.routes, ...customRoutes],
-};
